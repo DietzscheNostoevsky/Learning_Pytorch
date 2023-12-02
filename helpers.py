ce87dd38f5c9cb80682b7424d0d6edea043caebc
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2023-12-02 21:54:54 +0530

    documentation

--- helpers.py
+++ helpers.py
@@ -3,4 +3,20 @@
 A series of helper functions used throughout the course.
     
 If a function gets defined once and could be used over and over, it'll go in here.
- """
+"""
+
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+
+from torch import nn
+
+import os
+import zipfile
+
+from pathlib import Path
+import requests
+
+# Walk through an image classification directory and find out hoe many files (images)
+# are in each subdirectory.
+
