680ef76fe6cd1d17bf9ca98077ad72bb0bb75788
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2023-12-02 22:00:17 +0530

    walkthrough docstring

--- helpers.py
+++ helpers.py
@@ -20,3 +20,16 @@ import requests
 # Walk through an image classification directory and find out hoe many files (images)
 # are in each subdirectory.
 
+
+def walk_thorugh_dir(dir_path):
+    """
+    Walk through dir_path returning its contents
+    Args:
+    dir_path (str): target directory
+
+    Returns:
+    A print out of:
+        number of subdirectories in dir_path
+        number of images (files) in each subdirectory
+        number of each subdirectory
+    """
