2c75d5bb0a45b03a3668d2ab2da6afe6cfc56dd1
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2023-12-02 22:44:40 +0530

    decision boundary half done

--- helpers.py
+++ helpers.py
@@ -37,3 +37,18 @@ def walk_thorugh_dir(dir_path):
         print(
             f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'."
         )
+
+
+def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Tensor):
+    """
+    Plots decison boundaries of model predicting on X in comprison to y.
+
+    Source - https://madewithml.com/courses/foundations/neural-networks/ (with modifications)
+    """
+    # Put everything to CPU (works better with NumPy + Matplotlib)
+    model.to("cpu")
+    X, y = X.to("cpu"), y.to("cpu")
+
+    # Setup predictoin boundaries and grid
+    x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
+    y_min, Y_max = X[:, 1].min() - 0.1
